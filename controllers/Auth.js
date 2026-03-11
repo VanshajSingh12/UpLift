8cc937e108b340d6678cc65db0371520c09e77d8
VanshajSingh12 <vanshajsingh272@gmail.com>
2026-03-11 21:35:14 +0530

    Update Auth.js,  made signup controller

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcrypt")
 const User = require("../models/User")
 const OTP = require("../models/OTP")
 const jwt = require("jsonwebtoken")
@@ -55,3 +55,110 @@ exports.sendotp = async (req, res) => {
         return res.status(500).json({ success: false, error: error.message })
     }
 }
+
+//signup
+exports.signup = async (req, res) => {
+    try {
+        // fetch data from the request body
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp,
+        } = req.body
+        // Check if All Details are there or not
+        if (
+            !firstName ||
+            !lastName ||
+            !email ||
+            !password ||
+            !confirmPassword ||
+            !otp
+        ) {
+            return res.status(403).send({
+                success: false,
+                message: "All Fields are required",
+            })
+        }
+        // Check if password and confirm password match
+        if (password !== confirmPassword) {
+            return res.status(400).json({
+                success: false,
+                message:
+                    "Password and Confirm Password do not match. Please try again.",
+            })
+        }
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists. Please sign in to continue.",
+            })
+        }
+
+        // Find the most recent OTP for the email in DB // because 5 min (expiration time) ke andar bhi multiple otp bhej sakta hai ek user ko
+        const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1)//sorted on the basis of createdAt
+        console.log(response)
+        if (response.length === 0) {
+            // OTP not found for the email
+            return res.status(400).json({
+                success: false,
+                message: "The OTP is not valid",
+            })
+        } else if (otp !== response[0].otp) {
+            // Invalid OTP
+            return res.status(400).json({
+                success: false,
+                message: "The OTP is not valid",
+            })
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        // Create the user
+        let approved = ""
+        approved === "Instructor" ? (approved = false) : (approved = true)
+
+        // Create the Additional Profile For User , made because user require this data in additionalDetails section
+        const profileDetails = await Profile.create({
+            gender: null,
+            dateOfBirth: null,
+            about: null,
+            contactNumber: null,
+        })
+
+        //create user entry in DB
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            password: hashedPassword,
+            accountType: accountType,
+            approved: approved,
+            additionalDetails: profileDetails._id,
+            image: `https://api.dicebear.com/9.x/initials/svg?seed=${firstname} ${lastname}`,
+        })
+
+        return res.status(200).json({
+            success: true,
+            user,
+            message: "User registered successfully",
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be registered. Please try again",
+        })
+    }
+}
+
+
