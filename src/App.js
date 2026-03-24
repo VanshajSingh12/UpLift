b02a74b24cb86b18131bfa84b9749e44ca818efd
VanshajSingh12 <vanshajsingh272@gmail.com>
2026-03-24 16:28:50 +0530

    added assets

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <main class="flex justify-center gap-4 flex-col min-h-screen">
+            <h1>ABCD</h1>
+        </main>
+    );
+}
+
+export default App;
